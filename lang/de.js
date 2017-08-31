6d21bc1090bc41f6a54e4398548efc2fee57a87d
sheraz24 <sk@sherazkhan.de>
2017-08-31 20:22:56 +0200

    Text Chages
    - Updated text

--- lang/de.js
+++ lang/de.js
@@ -20,22 +20,19 @@ lang['de']['switchLanguage'] = "Changing the language will result in loosing all
 lang['de']['logoClick'] = "Wenn Sie die Seite neu laden verlieren Sie alle Angaben, die sie bisher gemacht haben\nReloading the page will cause loss of all answers given. Proceed?"
 
 // first page
-lang['de']['header'] = "Bundeswahl 2017";
+lang['de']['header'] = "Bundestagswahl 2017";
 lang['de']['js'] = '<strong>Ohne Java-Script geht hier leider nichts. Dafür berechnen wir auch alles nur auf deinem Gerät und senden deine Meinungen an keinen Server.<br />Deal?</strong>';
 
 lang['de']['einleitung'] =
- '<p><strong>Deine Antworten werden nur auf deinem Gerät (Computer, Handy, etc.) ausgewertet und an keinen Server gesendet.</strong> Das heißt aber auch, dass deine Antworten verloren gehen, sobald du diese Seite neu lädst oder verlässt.</p>'+
- '<p>Bei den Bundeswahl 2017 ...</p>'+
- '<p><strong>24. September 2017</strong><br />Info zum Wahlprozess, Info zum Briefwahl</p>'+
- '<p>Dies ist keine Wahlempfehlung, sondern nur ein Informationsangebot über aktuelle Positionen.</p>';
+ '<p>Ihre Antworten werden nur auf Ihrem Gerät (Computer, Handy, etc.) ausgewertet und an keinen Server gesendet. Das heißt aber auch, dass Ihre Antworten verloren gehen, sobald Sie diese Seite neu laden oder verlassen.</p>';
 
 lang['de']['losgehts'] = "los geht's!";
 
 // second page
-lang['de']['weighting'] = 'Was ist dir besonders wichtig?';
-lang['de']['gtxt'] ='<p>Hier kannst du beliebig viele Statements auswählen, die dir besonders wichtig sind. Sie werden dann doppelt gewichtet.</p>';
+lang['de']['weighting'] = 'Welche Thesen sind Ihnen besonders wichtig?';
+lang['de']['gtxt'] ='<p>Von Ihnen markierte Thesen werden bei der Auswertung doppelt gezählt.</p>';
 lang['de']['next'] = 'Weiter zur Auswertung';
 
 // third page
-lang['de']['result'] = '<p>Der studi-mat ergab für dich folgendes Ergebnis. Unten findest du zu jeder Frage die Postionen mit Begründungen (klick auf die Frage) der einzelnen Listen. Dies ist keine Wahlempfehlung, sondern nur ein Informationsangebot über die aktuellen Positionen in der Hochschulpolitik an der TU Darmstadt.</p>';
+lang['de']['result'] = '<p>Der Science-O-Mat ergab für dich folgendes Ergebnis. Unten findest du zu jeder Frage die Postionen mit Begründungen (klick auf die Frage) der einzelnen Listen. Dies ist keine Wahlempfehlung, sondern nur ein Informationsangebot über die aktuellen Positionen in der Hochschulpolitik an der TU Darmstadt.</p>';
 lang['de']['furtherinfos'] = '<h3>Weitere Informationen zur Wahl</h3><p>Viele weitere Infos zur diesjährigen Wahl und zur Hochschulpolitik an deiner Uni findest du in der Wahlzeitung und auf der Webseite deines AStA. <a href="https://www.asta.tu-darmstadt.de/asta/de/themen/hochschulwahlen">www.asta.tu-darmstadt.de/asta/de/themen/hochschulwahlen</a></p>';
